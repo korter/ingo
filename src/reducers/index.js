42a63fdf44e88f2ff4f80afa9b65c921c331dda3
pamentoro <pamentoro88@gmail.com>
2021-07-25 18:59:24 +0100

    Setting up Redux

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import gamesReducer from "./gamesReducer";
+import userReducer from "./userReducer";
+
+const rootReducer = combineReducers({
+  games: gamesReducer,
+  user: userReducer,
+});
+
+export default rootReducer;
