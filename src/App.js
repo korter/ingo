85e5eb676df8ae970c5c9bc4f23934cbe5c0ef7b
pamentoro <pamentoro88@gmail.com>
2021-07-25 19:37:04 +0100

    Action Creator, axios, fetching first data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "./actions/gamesAction";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  });
   return (
     <div>
       <h1>Hello Ingo</h1>
