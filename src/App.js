c04cc7093dcdea435987c5449f8e135b6c84859f
pamentoro <pamentoro88@gmail.com>
2021-07-26 09:19:14 +0100

    Added Pages and put the Fetch to Home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { loadGames } from "./actions/gamesAction";
+//Components and pages
+import Home from "./pages/Home";
 
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadGames());
-  });
   return (
     <div>
       <h1>Hello Ingo</h1>
+      <Hello />
     </div>
   );
 };
